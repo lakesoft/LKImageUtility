e39416f991eacb81f5b707d9953980cccffc4dcf
Hiroshi Hashiguchi <xcatsan@mac.com>
2014-05-15 12:42:53 +0900

    Initial Commit

---
+++ LKImageUtility/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LKImageUtility
+//
+//  Created by Hiroshi Hashiguchi on 2014/05/15.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
